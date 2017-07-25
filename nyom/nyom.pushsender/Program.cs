c2660a671ce96f6ce96f3f440b57930ed48c197c
helder.rezei <helder.rezei@concrete.com.br>
2017-07-25 12:15:37 -0300

    first commit

---
+++ nyom/nyom.pushsender/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nyom.pushsender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ nyom/nyom.queuebuilder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nyom.queuebuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
