1d69edc0966dbaaf1e2d26482ae10031d6061d59
alessandro.silveira <alessandro.silveira@concrete.com.br>
2017-07-27 18:31:57 -0300

    criado mecanismo de fila de mensagem

--- nyom/nyom.infra/Data/EntityFramwork/Context/NyomContextFactory.cs
+++ nyom/nyom.infra/Data/EntityFramwork/Context/NyomContextFactory.cs
@@ -1,23 +1,17 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.EntityFrameworkCore.Storage;
-using Microsoft.Extensions.Configuration;
 
 namespace nyom.infra.Data.EntityFramwork.Context
 {
-    public class NyomContextFactory : IDbContextFactory<NyomContext>
-    {
-		
-		
-	    public NyomContext Create(DbContextFactoryOptions options)
-	    {
-		    var optionsBuilder = new DbContextOptionsBuilder<NyomContext>();
-		   
-		    optionsBuilder.UseSqlServer("Server=localhost; Database=nyom; User ID=sa; Password=nyom.7410");
+	public class NyomContextFactory : IDbContextFactory<NyomContext>
+	{
+		public NyomContext Create(DbContextFactoryOptions options)
+		{
+			var optionsBuilder = new DbContextOptionsBuilder<NyomContext>();
 
-		    return new NyomContext(optionsBuilder.Options);
+			optionsBuilder.UseSqlServer("Server=localhost; Database=nyom; User ID=sa; Password=nyom.7410");
+
+			return new NyomContext(optionsBuilder.Options);
 		}
-       
-    }
+	}
 }
--- nyom/nyom.queuebuilder/Program.cs
+++ nyom/nyom.queuebuilder/Program.cs
@@ -1,26 +1,63 @@
 ﻿using System;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using nyom.infra.Data.EntityFramwork.Context;
+using System.Text;
+using nyom.domain.Notifications;
+using RabbitMQ.Client;
 
 namespace nyom.queuebuilder
 {
-    class Program
-    {
-	    public static IConfigurationRoot Configuration { get; set; }
-	    private static IServiceProvider _serviceProvider;
-		static void Main(string[] args)
-		{
-			var builder = new ConfigurationBuilder()
-				.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-			Configuration = builder.Build();
+	internal class Program
+	{
+		//public static IConfigurationRoot Configuration { get; set; }
+		//private static IServiceProvider _serviceProvider;
 
-			//setup DI
-			_serviceProvider = new ServiceCollection()
-				.AddDbContext<NyomContext>(o => o.UseSqlServer(Configuration["DefaultConnection"]))
-				.BuildServiceProvider();
+		
+		private static INotificationService _notificationService;
+
+		public Program(INotificationService notificationService)
+		{
 			
+			_notificationService = notificationService;
 		}
-    }
+
+		private static void Main(string[] args)
+		{
+			////var builder = new ConfigurationBuilder()
+			////	.AddJsonFile("appsettings.json", false, true);
+			////Configuration = builder.Build();
+
+
+			////_serviceProvider = new ServiceCollection()
+			////	.AddDbContext<NyomContext>(o => o.UseSqlServer(Configuration["DefaultConnection"]))
+			////	.BuildServiceProvider();
+
+			var dadosNotificacao = _notificationService.Find(a => a.CodigoNotificacao.Equals(0));
+
+			var factory = new ConnectionFactory {HostName = "localhost"};
+			using (var connection = factory.CreateConnection())
+			using (var channel = connection.CreateModel())
+			{
+				channel.QueueDeclare("hello",
+					false,
+					false,
+					false,
+					null);
+
+				
+				foreach (var arg in args)
+					for (var i = 0; i <= Convert.ToInt32(arg); i++)
+					{
+						var message = "Enviada a mensagem de numero: " + " " + i;
+						var body = Encoding.UTF8.GetBytes(message);
+						channel.BasicPublish("",
+							"hello",
+							null,
+							body);
+						
+					}
+			}
+
+			Console.WriteLine(" Press [enter] to exit.");
+			Console.ReadLine();
+		}
+	}
 }
