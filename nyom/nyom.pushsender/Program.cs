58b17919055c6431a887ef91b78ec67d7fa62d9f
helder.rezei <helder.rezei@concrete.com.br>
2017-08-03 18:32:15 -0300

    commit changes

--- nyom/nyom.pushsender/Program.cs
+++ nyom/nyom.pushsender/Program.cs
@@ -1,12 +1,61 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace nyom.pushsender
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory() { HostName = "rabbit", Port = 5672, UserName = "guest", Password = "guest" };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "CampanhaX",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: true,
+                                         arguments: null);
+
+                    channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+                    Console.WriteLine(" [*] Waiting for messages.");
+                    Console.WriteLine("Started " + DateTime.Now);
+
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] Received {0}", message);
+
+                        int dots = message.Split('.').Length - 1;
+                        Thread.Sleep(dots * 1000);
+
+                        Console.WriteLine(" [x] Done");
+
+                        channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                    };
+
+                    channel.BasicConsume(queue: "CampanhaX",
+                                         autoAck: false,
+                                         consumer: consumer);
+
+                    Console.WriteLine("Finished " + DateTime.Now);
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+
+                    Thread.Sleep(500000000);
+                }
+            }
         }
     }
 }
--- nyom/nyom.queuebuilder/Program.cs
+++ nyom/nyom.queuebuilder/Program.cs
@@ -1,12 +1,51 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
 
 namespace nyom.queuebuilder
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory() { HostName = "localhost", Port = 5672, UserName = "guest", Password = "guest" };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "CampanhaX", 
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: true,
+                                         arguments: null);
+
+                    Console.WriteLine(" [x] Sent {0}", args[0]);
+
+                    var limit = 0;
+
+                    while (limit < Convert.ToInt64(args[0]))
+                    {
+                        string message = "Teste do Helder ";
+                        message += limit.ToString();
+
+                        var body = Encoding.UTF8.GetBytes(message);
+
+                        var properties = channel.CreateBasicProperties();
+                        properties.Persistent = true;
+
+                        channel.BasicPublish(exchange: "",
+                                             routingKey: "CampanhaX",
+                                             basicProperties: null,
+                                             body: body);
+
+                        limit++;
+                    }
+                }
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
         }
     }
 }
