e167b67c548976c1385074e5e738a9fc48b71afc
alessandro.silveira <alessandro.silveira@concrete.com.br>
2017-08-07 10:04:00 -0300

    Revert "commit"
    This reverts commit 5d0bf1a553eb6f426db5219464ade982a1497ffc.

--- nyom/nyom.infra/Data/EntityFramwork/Context/WorkflowContextFactory.cs
+++ nyom/nyom.infra/Data/EntityFramwork/Context/WorkflowContextFactory.cs
@@ -10,7 +10,7 @@ namespace nyom.infra.Data.EntityFramwork.Context
 		{
 			var optionsBuilder = new DbContextOptionsBuilder<WorkflowContext>();
 
-			optionsBuilder.UseSqlServer("Server=localhost; Database=workflow; User ID=sa; Password=nyom.workflow-7410");
+			optionsBuilder.UseSqlServer("Server=mssql.workflow; Database=workflow; User ID=sa; Password=nyom.workflow-7410");
 
 			return new WorkflowContext(optionsBuilder.Options);
 		}
