8d0c56028b6e5088fe75d91c5ece383f933a3357
alessandro.silveira <alessandro.silveira@concrete.com.br>
2017-08-30 18:01:01 -0300

    commit

--- nyom/nyom.infra/CrossCutting/Helper/DockerHelper.cs
+++ nyom/nyom.infra/CrossCutting/Helper/DockerHelper.cs
@@ -3,60 +3,34 @@ using System.Diagnostics;
 
 namespace nyom.infra.CrossCutting.Helper
 {
-    public class DockerHelper : IDockerHelper
-    {
-        public void Run(Guid dadosCampanhaCampanhaId, string servico)
-        {
-    //        using (var processo = new Process())
-    //        {
-    //            processo.StartInfo.FileName = Environment.GetEnvironmentVariable("comspec");
-    //            processo.StartInfo.Arguments = string.Format(@"docker run {0} --alias={1}  --net {2} --links{3} -e CAMPANHA={1} -v tcp://docker.for.win.localhost:2375:/var/run/docker.sock", servico, dadosCampanhaCampanhaId, "dotnetcorerabbitmq_net.workflow", "mssql.workflow");
-				//Console.WriteLine(string.Format(@"docker run {0} --alias={1}  --net {2} --links {3} -e CAMPANHA={1} -v tcp://docker.for.win.localhost:2375:/var/run/docker.sock", servico, dadosCampanhaCampanhaId, "dotnetcorerabbitmq_net.workflow", "mssql.workflow"));
-				//processo.StartInfo.RedirectStandardOutput = true;
-    //            processo.StartInfo.UseShellExecute = false;
-    //            processo.StartInfo.CreateNoWindow = true;
-	   //         System.Diagnostics.Process.Start("CMD.exe", processo.StartInfo.Arguments);
-    //            //processo.Start();
-    //        }
+	public class DockerHelper : IDockerHelper
+	{
+		public void Run(Guid dadosCampanhaCampanhaId, string servico)
+		{
+			var argumento =
+				string.Format(
+					"docker run {0} --alias={1}  --net {2} --links {3} -e CAMPANHA={1} -v tcp://docker.for.win.localhost:2375:/var/run/docker.sock",
+					servico, dadosCampanhaCampanhaId, "net.workflow", "mssql.workflow");
+			Process.Start("cmd.exe", "/c " + argumento);
+		}
 
-	
-			var argumento = string.Format("docker run {0} --alias={1}  --net {2} --links {3} -e CAMPANHA={1} -v tcp://docker.for.win.localhost:2375:/var/run/docker.sock", servico, dadosCampanhaCampanhaId, "net.workflow", "mssql.workflow");
-	        System.Diagnostics.Process.Start("cmd.exe", "/c "+argumento);
-	       
+		public void Inspect(string servico)
+		{
+			var argumento = string.Format("docker inspect {0}", servico);
+			Process.Start("cmd.exe", "/c " + argumento);
+		}
 
-        }
+		public void Execute(string servico)
+		{
+			var argumento = string.Format("docker exec -d {0}", servico);
+			Process.Start("cmd.exe", "/c " + argumento);
+		}
 
-        public void Inspect(string servico)
-        {
-            using (var processo = new Process())
-            {
-                processo.StartInfo.FileName = Environment.GetEnvironmentVariable("comspec");
-                processo.StartInfo.Arguments = string.Format("docker inspect {0}", servico);
-                processo.StartInfo.RedirectStandardOutput = true;
-                processo.StartInfo.UseShellExecute = false;
-                processo.StartInfo.CreateNoWindow = true;
-                processo.Start();
-            }
-        }
-
-        public void Execute(string servico)
-        {
-            using (var processo = new Process())
-            {
-                processo.StartInfo.FileName = Environment.GetEnvironmentVariable("comspec");
-                processo.StartInfo.Arguments = string.Format("docker exec -d {0}", servico);
-                processo.StartInfo.RedirectStandardOutput = true;
-                processo.StartInfo.UseShellExecute = false;
-                processo.StartInfo.CreateNoWindow = true;
-                processo.Start();
-            }
-        }
-
-        public void CriarContainerDocker(Guid id, string servico)
-        {
-            Run(id, servico);
-            Inspect(servico);
-            Execute(servico);
-        }
-    }
+		public void CriarContainerDocker(Guid id, string servico)
+		{
+			Run(id, servico);
+			Inspect(servico);
+			Execute(servico);
+		}
+	}
 }
